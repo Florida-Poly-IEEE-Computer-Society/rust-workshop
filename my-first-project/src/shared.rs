d787541dba44cf8cbe7b7364dc0359823f522617
noah campise <noah@noah-MS-7E13.search.charter.net>
2025-09-14 21:25:43 -0400

    add structs and enumerations

--- my-first-project/src/shared.rs
+++ my-first-project/src/shared.rs
@@ -1,2 +1,101 @@
-// Shared file containing shared structs and enumerations throughout.
-// Please implement here
+use core::fmt;
+use std::collections::VecDeque;
+
+// Enumeration for the different token types that we are expecting
+// to push on to the stack
+#[derive(Debug, Clone)]
+pub enum Token {
+    Whole(f64),
+    Floating(u64, f64),
+    Operator(Operator),
+}
+
+#[derive(Debug, Clone)]
+pub enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    OpenParen,
+    CloseParen,
+}
+
+// OperatorStack is a type alias for a Vector of Tokens
+pub type OperatorStack = Vec<Operator>;
+
+impl Operator {
+    // Maps operators to u8 for comparison
+    // Used to compare order of operation
+    fn operator_order(&self) -> u8 {
+        match self {
+            Operator::Add => 1,
+            Operator::Sub => 1,
+            Operator::Mul => 2,
+            Operator::Div => 2,
+            Operator::CloseParen => 4,
+            Operator::OpenParen => 4,
+        }
+    }
+}
+
+// Trait used to compare operators with == and !=
+impl PartialEq for Operator {
+    fn eq(&self, other: &Self) -> bool {
+        self.operator_order() == other.operator_order()
+    }
+}
+
+// Trait used to compare order with <, >, >=, and <=
+impl PartialOrd for Operator {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self == other {
+            Some(std::cmp::Ordering::Equal)
+        } else if self.operator_order() > other.operator_order() {
+            Some(std::cmp::Ordering::Greater)
+        } else {
+            Some(std::cmp::Ordering::Less)
+        }
+    }
+}
+
+// OutputQueue used in the Shunting Yard Algorithm
+#[derive(Debug, Clone)]
+pub struct OutputQueue {
+    data: VecDeque<Token>,
+}
+
+// Implementation of methods for OutputQueue struct
+impl OutputQueue {
+    pub fn new() -> Self {
+        Self {
+            data: VecDeque::new(),
+        }
+    }
+    pub fn dequeue(&mut self) -> Option<Token> {
+        // Dequeue item at the front of the queue
+        self.data.pop_front()
+    }
+    pub fn queue(&mut self, val: Token) {
+        // Queue iten to the end of the queue
+        self.data.push_back(val);
+    }
+}
+
+// Implementing the display trait for OutputQueue
+// You only need to know that the trait std::fmt::Display is
+// used for macros like println!("") and to have your struct
+// usable for that macros, it needs to implement std::fmt::Display
+impl std::fmt::Display for OutputQueue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Queue values {:?}", self.data)
+    }
+}
+
+// Implement the iterator trait
+// This will be useful for calculating from postfix notation
+impl std::iter::Iterator for OutputQueue {
+    type Item = Token;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.dequeue()
+    }
+}
