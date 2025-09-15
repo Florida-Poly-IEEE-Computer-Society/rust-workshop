99fd6638d3b81a9119a0e3f95681e6bb0c04060d
noah campise <noah@noah-MS-7E13.search.charter.net>
2025-09-14 23:43:31 -0400

    finish project

--- my-first-project/src/main.rs
+++ my-first-project/src/main.rs
@@ -1,7 +1,22 @@
+use parse::parse;
+use solve_postfix::solve_postfix;
+
 mod parse;
 mod shared;
 mod shunting;
 mod solve_postfix;
 fn main() {
-    println!("You have the application. Congratulations!");
+    // Read in input.
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    println!("Welcome to the Rust Workshop Calculator Command Line Tool!\nPlease input a value:");
+    while stdin.read_line(&mut buffer).is_ok() {
+        let trimmed = buffer.trim_end();
+        let parsed = parse(trimmed);
+        let postfix = shunting::shunting_algo(parsed);
+        let solution = solve_postfix(postfix);
+        println!("{solution}");
+        println!("Please input a value:");
+        buffer = "".to_string();
+    }
 }
--- my-first-project/src/solve_postfix.rs
+++ my-first-project/src/solve_postfix.rs
@@ -1,2 +1,47 @@
-// Implement solve_postfix functionality here.
-// Please implment
+use crate::shared::*;
+
+// Take some Output_Queue stack, assuming that it is in postfix notation
+// And return a floating number
+pub fn solve_postfix(output_queue: OutputQueue) -> f64 {
+    // To solve in postfix, we will need a stack and logic to handle the difference cases
+    let mut stack: Vec<f64> = Vec::new();
+    // loop through each token in the queue
+    for i in output_queue {
+        match i {
+            Token::Whole(val) => stack.push(val),
+            Token::Floating(_, val) => stack.push(val),
+            Token::Operator(operator_type) => {
+                // The operation should go: x operation y
+                let x = stack.pop().expect("Stack should not be empty");
+                let y = stack.pop().expect("Stack should not be empty");
+                match operator_type {
+                    Operator::Add => stack.push(add_floats(x, y)),
+                    Operator::Sub => stack.push(substract_floats(x, y)),
+                    Operator::Mul => stack.push(multiply_floats(x, y)),
+                    Operator::Div => stack.push(divide_floats(x, y)),
+                    _ => panic!("Unexpected operator"),
+                }
+            }
+        }
+    }
+    stack
+        .pop()
+        .expect("Calculation finished, should have succeeded")
+}
+
+// Project functions that students will need to work on in order to fix their calculator
+fn add_floats(x: f64, y: f64) -> f64 {
+    x + y
+}
+
+fn substract_floats(x: f64, y: f64) -> f64 {
+    y - x
+}
+
+fn multiply_floats(x: f64, y: f64) -> f64 {
+    x * y
+}
+
+fn divide_floats(x: f64, y: f64) -> f64 {
+    y / x
+}
